555ff31bc628d499f91d1023f80be5b67312f4f6
PranaliAdsul <pranuadsulst1137@gmail.com>
2023-05-21 03:18:18 +0530

    fitness_tracker cleared scrab

--- my_fitness_tracker/src/index.js
+++ my_fitness_tracker/src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div id="root"></div>
     <App />
   </React.StrictMode>
 );
-
-reportWebVitals();
+ReactDOM.createRoot(document.getElementById('root'));
